3778e0dc91a4d276b892e3cbacc49fbacb8661f3
2181025743 <2181025743@qq.com>
2025-08-18 17:19:59 +0800

    删除操作

--- day05/src/main/java/com/yx/mapper/StudentMapper.java
+++ day05/src/main/java/com/yx/mapper/StudentMapper.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface StudentMapper {
     List<Student> selectByCondition(Student student);
+
+    List<Student> selectByConditionTrim(Student student);
+
+    boolean update(Student student);
+
+    int deleteList(List<Integer> ids);
 }
--- day05/src/main/java/com/yx/test/TestTrim.java
+++ day05/src/main/java/com/yx/test/TestTrim.java
@@ -8,23 +8,14 @@ import org.apache.ibatis.session.SqlSession;
 import java.io.IOException;
 import java.util.List;
 
-public class Test {
+public class TestTrim {
     public static void main(String[] args) throws IOException {
         SqlSession session = SessionUtil.session(true);
         StudentMapper mapper = session.getMapper(StudentMapper.class);
-
         Student condition = new Student();
+
         condition.setName("杜");
-        List<Student> result1 = mapper.selectByCondition(condition);
+        List<Student> result1 = mapper.selectByConditionTrim(condition);
         result1.forEach(System.out::println);
-        System.out.println("1111111");
-        condition.setGender("男");
-        List<Student> result2 = mapper.selectByCondition(condition);
-        result2.forEach(System.out::println);
-        System.out.println("222222");
-        condition.setAge(21);
-        List<Student> result3 = mapper.selectByCondition(condition);
-        result3.forEach(System.out::println);
-        System.out.println("333");
     }
 }
